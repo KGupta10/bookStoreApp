aea338e5ec388036e4577b44954201b671228017
Kavya <92774828+KGupta10@users.noreply.github.com>
2024-11-22 06:13:01 -0800

    frontend

---
+++ frontend/src/components/Freebook.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Slider from "react-slick";
+
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import list from "../../public/list.json"
+import Cards from './Cards';
+
+function Freebook() {
+    const filterData = list.filter((data)=>data.category==="Free");
+    console.log(filterData);
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
+  return (
+    <>
+        <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+            <div>
+                <h1 className='font-semibold text-xl pb-2'>Free Offered Courses</h1>
+                <p>this is some info text that maybe i'll fill later, maybe i wont! who knows, def not me !!
+                    lorem ipsum lorem ipsum filler filler text text when will this be enough to fill it
+                    to the second line
+                </p>
+            </div>
+        
+            <div>
+                <Slider {...settings}>
+                    {filterData.map((item)=>(
+                        <Cards item={item} key={item.id}/>
+                    ))}
+                </Slider>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Freebook
